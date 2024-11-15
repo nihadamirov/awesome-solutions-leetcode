13d2da80b02321bc495d4a360765b0d15b8d83e9
nihadamirov <nihadamirov98@gmail.com>
2024-11-15 23:52:19 +0400

    solved: use floyd’s cycle detection algorithm

---
+++ src/leetcode/linkedlist/LinkedListCycle.java
@@ -0,0 +1,35 @@
+package leetcode.linkedlist;
+
+public class LinkedListCycle {
+    public static void main(String[] args) {
+        ListNode head = new ListNode(3);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(0);
+        head.next.next.next = new ListNode(-4);
+        head.next.next.next.next = head.next;
+
+        boolean hasCycle = hasCycle(head);
+
+        System.out.println(hasCycle);
+    }
+
+    static boolean hasCycle(ListNode head) {
+        if (head == null) {
+            return false;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
